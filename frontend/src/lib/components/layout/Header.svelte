0de8c9a3dc6727287ab7bfc9e81bf8b667b1d023
Derrick Persson <derrickpersson@gmail.com>
2023-11-22 12:02:32 -0800

    Set up the basics for the web app (#3)

---
+++ frontend/src/lib/components/layout/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { AppBar } from '@skeletonlabs/skeleton';
+	import Logo from "./Logo.svelte";
+
+</script>
+<AppBar>
+    <Logo />
+</AppBar>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Header from "$lib/components/layout/Header.svelte";
+import "../app.pcss";
+    import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header"><Header /></svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
